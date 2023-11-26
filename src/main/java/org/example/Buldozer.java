205b01a4fb71f2dfe5326da6f5e7abdd86b6a275
Vitaliy <vitaliyufc@gmail.com>
2023-11-27 00:08:39 +0300

    Done homework carsOOP

---
+++ src/main/java/org/example/Buldozer.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Buldozer extends Machine
+{
+    @Override
+    public void doWork()  { System.out.println("Buldozer started its work.");}
+
+
+    @Override
+    public void stopWork() { System.out.println("Buldozer stopped working.");}
+
+}
